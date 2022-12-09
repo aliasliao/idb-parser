24cc88c2afc887dbba2506e13a9010a7d3fb513a
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2022-12-09 22:41:40 +0800

    indent with space

--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"log"
+    "fmt"
+    "log"
 
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/opt"
+    "github.com/syndtr/goleveldb/leveldb"
+    "github.com/syndtr/goleveldb/leveldb/opt"
 )
 
 func main() {
-	dbPath := "./data/IndexedDB/https_web.haiserve.com_0.indexeddb.leveldb"
+    dbPath := "./data/IndexedDB/https_web.haiserve.com_0.indexeddb.leveldb"
 
-	options := opt.Options{
-		Comparer:       bytesComparer{},
-		ErrorIfExist:   false,
-		ErrorIfMissing: true,
-		ReadOnly:       true,
-	}
-	db, err := leveldb.OpenFile(dbPath, &options)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
+    options := opt.Options{
+        Comparer:       bytesComparer{},
+        ErrorIfExist:   false,
+        ErrorIfMissing: true,
+        ReadOnly:       true,
+    }
+    db, err := leveldb.OpenFile(dbPath, &options)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    defer db.Close()
 
-	iter := db.NewIterator(nil, nil)
-	for iter.Next() {
-		// Remember that the contents of the returned slice should not be modified, and
-		// only valid until the next call to Next.
-		key := iter.Key()
-		value := iter.Value()
-		fmt.Printf("key=%v\nvalue=%v\n\n", string(key), string(value))
-	}
-	iter.Release()
-	err = iter.Error()
-	if err != nil {
-		log.Fatalln(err)
-	}
+    iter := db.NewIterator(nil, nil)
+    for iter.Next() {
+        // Remember that the contents of the returned slice should not be modified, and
+        // only valid until the next call to Next.
+        key := iter.Key()
+        value := iter.Value()
+        fmt.Printf("key=%v\nvalue=%v\n\n", string(key), string(value))
+    }
+    iter.Release()
+    err = iter.Error()
+    if err != nil {
+        log.Fatalln(err)
+    }
 }
